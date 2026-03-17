7e579b24a862bcc1e51dd7136b0745a974961289
Rich Gedeon <rgedeon@gmail.com>
2026-03-18 00:12:39 +0530

    Fix firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -8,8 +8,7 @@ const firebaseConfig = {
   projectId: "treasury-777",
   storageBucket: "treasury-777.firebasestorage.app",
   messagingSenderId: "1029157530169",
-  appId: "1:1029157530169:web:de6a2a19cfc10285077be8",
-  measurementId: "G-W0JFFSKGLL"
+  appId: "1:1029157530169:web:de6a2a19cfc10285077be8"
 };
 
 const app = initializeApp(firebaseConfig);
